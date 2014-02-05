d73f0ab816bcdd87e93f3ddb204d15f74508218d
Ben Cox <ben@benjojo.co.uk>
2014-02-05 13:58:39 +0000

    Added "Base" of the packet

--- aprs.go
+++ aprs.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+)
+
+type APRSPacket struct {
+	Callsign      string
+	PacketType    string
+	Latitude      string
+	Longitude     string
+	Altitude      string
+	GPSTime       string
+	RawData       string
+	Symbol        string
+	Heading       string
+	PHG           string
+	Speed         string
+	Destination   string
+	Status        string
+	WindDirection string
+	WindSpeed     string
+	WindGust      string
+	WeatherTemp   string
+	RainHour      string
+	RainDay       string
+	RainMidnight  string
+	Humidity      string
+	Pressure      string
+	Luminosity    string
+	Snowfall      string
+	Raincounter   string
+	Error         string
+}
